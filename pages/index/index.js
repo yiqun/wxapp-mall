1983a3faaaff91f28c2493aa3e0d6bee8e18a0f2
Michael Luk <michaelluk503@gmail.com>
2017-04-13 17:28:39 +0800

    add taobao, jd style index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,138 +1,59 @@
-const App = getApp()
+//index.js
+const index = require('../../controllers/indexController.js').controller
+
+//获取应用实例
+var app = getApp()
 
 Page({
-    data: {
-        activeIndex: 0,
-        navList: [],
-        indicatorDots: !0,
-        autoplay: !1,
-        current: 0,
-        interval: 3000,
-        duration: 1000,
-        circular: !0,
-        goods: {},
-        prompt: {
-            hidden: !0,
-        },
-    },
-    swiperchange(e) {
-        // console.log(e.detail.current)
-    },
-    onLoad() {
-        this.banner = App.HttpResource('/banner/:id', {id: '@id'})
-        this.goods = App.HttpResource('/goods/:id', {id: '@id'})
-        this.classify = App.HttpResource('/classify/:id', {id: '@id'})
-
-        this.getBanners()
-        this.getClassify()
-    },
-    initData() {
-        const type = this.data.goods.params && this.data.goods.params.type || ''
-        const goods = {
-            items: [],
-            params: {
-                page : 1,
-                limit: 10,
-                type : type,
-            },
-            paginate: {}
+  data: {
+    topScroll:[],
+    footerNav:[],
+    bannerIcon:[],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    pageIndex:1,
+    pageSize:10,
+    loading: true,
+    loadtitle:"康康正在努力为你加载更多...",
+    hasMore:true,
+    guessProducts:[]
+  },  
+  loadMoreGuessLike:function(){
+      if (!this.data.hasMore) return
+      this.setData({ loadtitle: '康康正在努力为你加载更多...', loading: true })
+      index.find(this.data.page++, this.data.size).then(d => {
+        if (d.PageIndex<TotalPage) {
+          this.setData({ loadtitle:'康康正在努力为你加载更多...',guessProducts: d.Data, loading: false })
+        } else {
+          this.setData({ loadtitle: '', hasMore: false, loading: false })
         }
-
-        this.setData({
-            goods: goods
-        })
-    },
-    navigateTo(e) {
-        console.log(e)
-        App.WxService.navigateTo('/pages/goods/detail/index', {
-            id: e.currentTarget.dataset.id
-        })
-    },
-    search() {
-        App.WxService.navigateTo('/pages/search/index')
-    },
-    getBanners() {
-    	// App.HttpService.getBanners({is_show: !0})
-        this.banner.queryAsync({is_show: !0})
-        .then(data => {
-        	console.log(data)
-        	if (data.meta.code == 0) {
-                data.data.items.forEach(n => n.path = App.renderImage(n.images[0].path))
-        		this.setData({
-                    images: data.data.items
-                })
-        	}
-        })
-    },
-    getClassify() {
-        const activeIndex = this.data.activeIndex
-
-        // App.HttpService.getClassify({
-        //     page: 1, 
-        //     limit: 4, 
-        // })
-        this.classify.queryAsync({
-            page: 1, 
-            limit: 4, 
-        })
-        .then(data => {
-            console.log(data)
-            if (data.meta.code == 0) {
-                this.setData({
-                    navList: data.data.items,
-                    'goods.params.type': data.data.items[activeIndex]._id
-                })
-                this.onPullDownRefresh()
-            }
-        })
-    },
-    getList() {
-        const goods = this.data.goods
-        const params = goods.params
-
-        // App.HttpService.getGoods(params)
-        this.goods.queryAsync(params)
-        .then(data => {
-            console.log(data)
-            if (data.meta.code == 0) {
-                data.data.items.forEach(n => n.thumb_url = App.renderImage(n.images[0] && n.images[0].path))
-                goods.items = [...goods.items, ...data.data.items]
-                goods.paginate = data.data.paginate
-                goods.params.page = data.data.paginate.next
-                goods.params.limit = data.data.paginate.perPage
-                this.setData({
-                    goods: goods,
-                    'prompt.hidden': goods.items.length,
-                })
-            }
-        })
-    },
-    onPullDownRefresh() {
-        console.info('onPullDownRefresh')
-        this.initData()
-        this.getList()
-    },
-    onReachBottom() {
-        console.info('onReachBottom')
-        if (!this.data.goods.paginate.hasNext) return
-        this.getList()
-    },
-    onTapTag(e) {
-        const type = e.currentTarget.dataset.type
-        const index = e.currentTarget.dataset.index
-        const goods = {
-            items: [],
-            params: {
-                page : 1,
-                limit: 10,
-                type : type,
-            },
-            paginate: {}
-        }
-        this.setData({
-            activeIndex: index,
-            goods: goods,
-        })
-        this.getList()
-    },
+      })
+      .catch(e => {
+        this.setData({ loadtitle: '获取数据异常', loading: false })
+        console.error(e)
+      })
+  },
+   onLoad: function () {
+    console.log('onLoad')
+    var _this = this
+    index.getTopScroll().then(d=>_this.setData({ topScroll : d }))
+    index.getBannerIcon().then(d=>_this.setData({ bannerIcon : d }))
+    index.getFooter().then(d=>_this.setData({ footerNav : d }))
+    _this.loadMoreGuessLike()
+  },
+  onReady () {
+      var _this = this
+      index.getFooter2().then(d=>_this.setData({ footerNav : d }))
+      // 页面渲染完成
+      wx.setNavigationBarTitle( {
+          title: "首页-]商城"
+      })
+  },
+  onShow () {
+      wx.setNavigationBarTitle( {
+          title: "首页-商城"
+      })
+  },
 })
